a9ff5e64a30b85dffce28cbfb6a42ff6854f4525
Kwame Efah <kwameefah@dhcp-18-189-17-16.dyn.MIT.EDU>
2015-11-16 22:10:58 -0500

    Integrate, mongo, mongoose, and mocha frameworks

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 var express = require('express'),
     http = require('http');
-
+    mongoose = require('mongoose');
 
 var app = express();
 
@@ -13,6 +13,21 @@ require('./config/bootup')(app);
 // Configure routes
 require('./config/routes')(app);
 
+// Start web server
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+/*
+Start database server
+TODO: Setup mongo on server +  create (/data/db) directory
+Remember to get db to work, /data/db must be created and 
+mongod command run 
+*/
+var mongoDirectory = '/data/db'
+
+mongoose.connect('mongodb://localhost:27017/pmidb');
+var db = mongoose.connection;
+db.on("error", function(err) {console.log("Mongoose error:", err)});
+
+
