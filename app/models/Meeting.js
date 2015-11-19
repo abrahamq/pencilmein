2e0ed31d07bd2033dd1eaf0cf8a7f376296f9a24
Rebecca Corcillo <corcillo@mit.edu>
2015-11-19 18:44:12 -0500

    added initial meeting schema

--- app/models/Meeting.js
+++ app/models/Meeting.js
@@ -1,1 +1,23 @@
 //Meeting
+var mongoose = require('mongoose');
+var User = require('./User.js');
+
+var MeetingSchema = mongoose.Schema({
+	title: String,
+	location: String,
+	duration: Number,
+	invitedMembers:[String],
+	respondedMembers: [String]
+});
+MeetingSchema.methods=
+{
+	isClosed : function(){
+		return this.invitedMembers.length == this.respondedMembers.length;
+	},
+	inviteMember : function(username){
+		this.invitedMembers.push(username);
+	},
+	recordMemberResponse: function(username){
+		this.respondedMembers.push(username);
+	}
+};
