83163deba9a590b9abd92a0fabdb9590afc57ae1
Rebecca Corcillo <corcillo@mit.edu>
2015-11-19 20:13:16 -0500

    did time block data model

--- app/models/TimeBlock.js
+++ app/models/TimeBlock.js
@@ -1,31 +1,15 @@
-var TimeBlock = function(){
-    var that = Object.create(TimeBlock.prototype);
-    //----------------------private variables-----------------------
-    var color = 'green';
-    var setType = 'calendar';
-    var isValidColor = function(newColor){
-        return (newColor=='green' || newColor=='yellow' || newColor == 'red');
-    }
-    var isValidSetType = function(newSetType){
-        return (newColor=='calendar' || newColor=='general' || newColor == 'manual');
-    }
-    //----------------------public functions-----------------------
-    that.getColor = function(){
-        return color;   
-    }
-    that.setColor = function(newColor){
-        if isValidColor(newColor){
-            color = newColor;
-        }
-    }
-    that.getSetType = function(){
-        return color;   
-    }
-    that.setSetType = function(newSetType){
-        if isValidSetType(newSetType){
-            setType = newSetType;
-        }
-    }
-    Object.freeze(that);
-    return that;    
-}
+var mongoose = require('mongoose');
+var TimeBlockSchema = mongoose.Schema({
+    color : {
+        type: String,
+        enum : ['green','yellow','red'],
+        default : 'green'
+    },
+    creationType : {
+        type: String,
+        enum : ['calendar','general','manual'],
+        default : 'calendar'
+    },
+    startDate : Date
+});
+module.exports = mongoose.model('TimeBlock', TimeBlockSchema);
