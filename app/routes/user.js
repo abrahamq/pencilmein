cb30e8033a973819bdf47611130d4a26b2426d19
Abraham Quintero <abrahamq@mit.edu>
2015-11-21 01:56:28 -0500

    preliminary calendar

--- app/routes/user.js
+++ app/routes/user.js
@@ -10,14 +10,30 @@ router.get('/', function(req, res) {
 
   User.getUser(req.user.googleEmail, function(err, user_orig) {
     user_orig.populate('meetings', function(err, user) {
-    if (err) {
-      utils.sendErrResponse(res, 400, 'User meetings do not exist');
-    }
-    else {
-      utils.renderTemplate(res, 'useroverview', {meetings: user.meetings, userName: req.user.fullname});
-    }
-   });
+      if (err) {
+        utils.sendErrResponse(res, 400, 'User meetings do not exist');
+      }
+      else {
+        utils.renderTemplate(res, 'useroverview', {meetings: user.meetings, userName: req.user.fullname});
+      }
+    });
   });
 });
+
+router.get('/availability', function(req, res){
+  var meetingId = req.params.meetingId; 
+  //res.render('calendarView'); 
+  utils.renderTemplate(res, 'calendarView'); 
+//  User.getUser(req.user.googleEmail, function(err, user_orig) {
+//    user_orig.populate('meetings', function(err, user) {
+//      if (err) {
+//        utils.sendErrResponse(res, 400, 'User meetings do not exist');
+//      }
+//      else {
+//        utils.renderTemplate(res, 'useroverview', {meetings: user.meetings, userName: req.user.fullname});
+//      }
+//    });
+//  });
+}); 
 
 module.exports = router; 
