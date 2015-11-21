6da98a480310bb300f259cbcf64836dc0e62daf9
Caroline Chin <cmchin@mit.edu>
2015-11-21 09:37:40 -0500

    starting code for finding an In

--- app/javascripts/optimeet.js
+++ app/javascripts/optimeet.js
@@ -1,5 +1,24 @@
 var optimeet = (function() {
+  
+
+
   var _optimeet = {}
+
+  var NUM_MILISECONDS_IN_MINUTE = 60000;
+
+  _optimeet.findIn = function(availabilities, mtg_startDate, mtg_duration) {
+    var num_slots = availabilities[0].length;
+    var possible_in = [];
+    var if_need_be = [];
+  }
+
+  var initialize_possible_in = function(startDate, duration, num_slots) {
+
+  }
+
+  var block_num_to_time = function(block_num, startDate) {
+
+  }
 
 
   Object.freeze(_optimeet);
