0df3a97c3db0c200ed5fe77d9920149908dd5569
Kwame Efah <kwameefah@dhcp-18-189-41-229.dyn.MIT.EDU>
2015-11-23 00:14:14 -0500

    meetings on user overview page redirects to calendar view

--- public/javascripts/userPage.js
+++ public/javascripts/userPage.js
@@ -6,12 +6,3 @@ $(document).on('click', '#schedule', function(e)
   window.location.replace('/meeting');
 });
 
-/*
-  When a meeting is clicked on the home page, redirect to the calendar view 
-  for that meeting 
-*/
-$('.list-group-item').on('click', function(e)
-{
-  var meetingId = $(this).id;
-  
-});
