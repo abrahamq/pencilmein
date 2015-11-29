8a79e3da3b86e82c22e2a81448c4b561f0551612
Kwame Efah <kwameefah@dhcp-18-111-79-198.dyn.MIT.EDU>
2015-11-29 14:41:27 -0500

    Code review changes from email handling

--- app/models/Meeting.js
+++ app/models/Meeting.js
@@ -43,7 +43,7 @@ MeetingSchema.methods=
 	*/
 	recordMemberResponse: function(googleID, cb){
 		if (this.respondedMembers.indexOf(googleID) == -1){
-			this.respondedMembers.push(googleID);
+      this.respondedMembers.push(googleID);
 		}
 		this.save(cb);
 	},
@@ -62,7 +62,6 @@ MeetingSchema.methods=
   */
   getInviteeEmailList: function(cb) {
   	var invitedMembers = this.invitedMembers;
-  	console.log(invitedMembers);
   	User.find({'_id' : { $in: invitedMembers}}, 'googleEmail', function(err,email) {
   		cb(err, email);
   	});
--- app/routes/index.js
+++ app/routes/index.js
@@ -29,7 +29,7 @@ if (!process.env.PRODUCTION){
 }
 
 //Google Authentication 
-router.get('/auth/google', passport.authenticate('google', { scope : ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/userinfo.profile','profile','email'], accessType: 'offline', approvalPrompt: 'auto' }));
+router.get('/auth/google', passport.authenticate('google', { scope : ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/userinfo.profile','profile','email'], accessType: 'offline', approvalPrompt: 'force' }));
 
 // the callback after google has authenticated the user
 router.get('/auth/google/callback',
