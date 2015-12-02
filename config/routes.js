f0b3458a556098972f0ef6aba0dd8d0a72036baa
Kwame Efah <kwameefah@dhcp-18-189-74-160.dyn.mit.edu>
2015-12-02 00:00:49 -0500

    General Preferences Front End

--- config/routes.js
+++ config/routes.js
@@ -5,8 +5,10 @@ module.exports = function(app) {
   var about = require('../app/routes/about'); 
 	var users = require('../app/routes/users');
   var meetings = require('../app/routes/meetings');
+  var preferences = require('../app/routes/preferences');
   app.use('/', index);
 	app.use('/about', about);
   app.use('/users', users);
+  app.use('/users/preferences', preferences);
   app.use('/meetings', meetings);
 }
